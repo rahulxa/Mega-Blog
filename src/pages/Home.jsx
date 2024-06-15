09a710bf94904706861aa3b600ae28dd7d8ab7f2
rahulxa <rahulac.5000@gmail.com>
2024-06-15 19:41:13 -0400

    update Home page and add post page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,12 +13,36 @@ function Home() {
                     setPosts(posts.documents)
                 }
             })
-    }, [third])
+    }, [])
 
 
     if (posts.length === 0) {
-        return 
-    }
+        return (
+            <div className='w-full py-8 mt-4 text-center'>
+                <Container>
+                    <div className='flex flex-wrap'>
+                        <div className='p-2 w-full'>
+                            <h1 className='text-2xl font-bold hover:text-gray-500'>
+                                Login to read Posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    } return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) =>
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <Postcard {...post} />
+                        </div>
+                    )}
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default Home
