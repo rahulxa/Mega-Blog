d84481d1a8b8bacd9eaabc9ee8709215f72ccb79
rahulxa <rahulac.5000@gmail.com>
2024-06-14 21:37:30 -0400

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import service from '../appwrite/config'
+import { Container, Postcard } from '../components'
 
 function Home() {
-    return (
-        <div>Home</div>
-    )
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        service.getPosts()
+            .then((posts) => {
+                if (posts) {
+                    setPosts(posts.documents)
+                }
+            })
+    }, [third])
+
+
+    if (posts.length === 0) {
+        return 
+    }
 }
 
 export default Home
